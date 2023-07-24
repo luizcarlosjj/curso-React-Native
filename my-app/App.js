f790e3b54dfb29233af513ecd381566bbee8ba28
Luiz Carlos <dia.a.diajj@gmail.com>
2023-07-24 16:05:21 -0300

    up

--- my-app/App.js
+++ my-app/App.js
@@ -4,32 +4,29 @@ import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 class App extends Component{
 
-  constructor(props){
-    super(props);
-    this.state = {
-      nome: ''
-    }
-    this.entrar = this.entrar.bind(this)
-  }
-
-  entrar() {
-    this.setState({
-      nome: 'Junior'
-    })
-  }
 
   render() {
     return(
-      <View style={{ marginTop: 40 }}>
-
-        <Text style={{ fontSize: 23, color: 'red', textAlign: 'center'}}> 
-          {this.state.nome} 
-        </Text>
-
-        <Button title='Entrar' onPress={this.entrar} />
+      <View style={styles.container}>
+        <Text style={styles.textoPrincipal}> Eu sou o texto 1 </Text>
+        <Text>Eu sou o texto 2</Text>
+        <Text>Eu sou o texto 3</Text>
+        <Text>Eu sou o texto 4</Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container :{
+    margin: 40,
+    flex: 1,
+  },
+  textoPrincipal:{
+    fontSize: 25,
+    color: "red"
+    
+  }
+});
 
 export default App;

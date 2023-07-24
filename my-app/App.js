29aeff0adc9bb2aa9aff0205450e8e2d396f7ba5
Luiz Carlos <dia.a.diajj@gmail.com>
2023-07-24 17:14:02 -0300

    aligns

--- my-app/App.js
+++ my-app/App.js
@@ -6,9 +6,14 @@ class App extends Component{
 
   render() {
     return(
-      <View style={{flex: 1,backgroundColor: '#222'}}>
-        <View style={{flex: 1, backgroundColor: 'red'}}></View>
-        <View style={{flex: 1, backgroundColor: 'green'}}></View>
+      <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+
+        <View style={{width:50 ,height: 50, backgroundColor: 'red'}}></View>
+
+        <View style={{width:50 ,height: 50, backgroundColor: 'green'}}></View>
+
+        <View style={{width:50 ,height: 50, backgroundColor: 'yellow'}}></View>
+
       </View>
     )
   }

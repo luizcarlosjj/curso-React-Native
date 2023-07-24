38fc44eeff966612370c9c633d7b899f076d296d
Luiz Carlos <dia.a.diajj@gmail.com>
2023-07-24 12:22:29 -0300

    App.js update

--- my-app/App.js
+++ my-app/App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Olá ! Seja bem vindo ao meu App!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component{
+  render() {
+
+    let nome = 'Luiz';
+    let img = 'https://www.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej.png';
+
+    return(
+      <View>
+        <Text style={{color:'#ff0000', fontSize: 25, margin: 50}}>
+          Olá Mundo
+        </Text>
+
+        <Image 
+          source={{ uri: img}}
+          style={{width: 300, height: 300}}
+        />
+
+        <Text style={{fontSize: 50, padding: 50}}>
+          {nome}
+        </Text>
+
+      </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;

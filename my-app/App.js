5144d8b080bfa4c3e6fa279124b4954f5e1487d2
Luiz Carlos <dia.a.diajj@gmail.com>
2023-07-24 16:40:10 -0300

    u

--- my-app/App.js
+++ my-app/App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
@@ -8,25 +7,10 @@ class App extends Component{
   render() {
     return(
       <View style={styles.container}>
-        <Text style={styles.textoPrincipal}> Eu sou o texto 1 </Text>
-        <Text>Eu sou o texto 2</Text>
-        <Text>Eu sou o texto 3</Text>
-        <Text>Eu sou o texto 4</Text>
+
       </View>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container :{
-    margin: 40,
-    flex: 1,
-  },
-  textoPrincipal:{
-    fontSize: 25,
-    color: "red"
-    
-  }
-});
 
 export default App;

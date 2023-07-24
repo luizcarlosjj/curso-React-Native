90e1270cf8a938a72a4f3047b4ef5d392e2fea85
Luiz Carlos <dia.a.diajj@gmail.com>
2023-07-24 16:54:13 -0300

    flex

--- my-app/App.js
+++ my-app/App.js
@@ -6,8 +6,9 @@ class App extends Component{
 
   render() {
     return(
-      <View style={styles.container}>
-
+      <View style={{flex: 1,backgroundColor: '#222'}}>
+        <View style={{flex: 1, backgroundColor: 'red'}}></View>
+        <View style={{flex: 1, backgroundColor: 'green'}}></View>
       </View>
     )
   }
